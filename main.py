80740e66523518970ccd6c960bd29ab504098e99
JonasFiechter <86444559+JonasFiechter@users.noreply.github.com>
2022-10-18 17:58:32 -0300

    Using Windows
    class-based Window structure to manipulate screens.

--- main.py
+++ main.py
@@ -2,22 +2,39 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.logger import Logger
 from kivy.lang import Builder
+from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.event import EventDispatcher
 
-Builder.load_file('./src/features/home/screens/home.kv')
 
-class TagReader(Widget):
+class MenuScreen(Screen, App):
+    def close_app(self):
+        Logger.error('INSIDE CLOSE_APP')
+        # TODO: consider adding a quit button into menu screen
+        self.stop()
+
+class SettingsScreen(Screen):
+    def log_on_screen(self):
+        Logger.warning('INSIDE SETTINGS')
+
+class TagReader(Screen):
     pass
 
-class TagApp(App):
+class TagReaderApp(App):
     # Load screen
     try:
-        print('testing')
+        print('test')
+        Builder.load_file('./src/features/home/screens/home.kv')
     except:
         Logger.error(f'loadfile error')
+
     def build(self):
-        self.title = 'Tag Reader'
-        return TagReader()
+        self.title = 'TagReaderApp'
+        sm = ScreenManager()
+        sm.add_widget(MenuScreen(name='Menu'))
+        sm.add_widget(SettingsScreen(name='Settings'))
+        sm.add_widget(TagReader(name='TagReader'))
+        return sm
 
 
 if __name__ == '__main__':
-    TagApp().run()
+    TagReaderApp().run()
